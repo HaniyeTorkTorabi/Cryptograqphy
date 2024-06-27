8afc4373173378f4a88c18cd5fcb4027b5e2c912
HaniyeTorkTorabi <htorktorabi@gmail.com>
2024-06-27 13:30:52 +0330

    get inputs

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the secret (S): ");
+        int secret = input.nextInt();
+
+        System.out.print("Enter the threshold (t): ");
+        int t = input.nextInt();
+
+        System.out.print("Enter the total number of shares (n): ");
+        int n = input.nextInt();
+
+        System.out.print("Enter the prime number (p): ");
+        int prime = input.nextInt();
+
+        input.close();
+    }
+
 }
